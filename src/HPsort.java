ecbed8d3fdd5ae8103d955012ec3babba9ca673e
mnmcalli <89427496+mnmcalli@users.noreply.github.com>
2021-12-08 17:30:01 -0500

    Update HPsort.java

--- src/HPsort.java
+++ src/HPsort.java
@@ -1,14 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
 
 public class HPsort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Quiz HPquiz = new Quiz();
-		HPquiz.read("questions.txt", 0);
-		HPquiz.read("Ganswers.txt", 1);
-		HPquiz.read("Ranswers.txt", 2);
-		HPquiz.read("Hanswers.txt", 3);
-		HPquiz.read("Sanswers.txt", 4);
+		Quiz HPquiz = new Quiz();       //Question(String q, String gA, String rA, String hA, String sA)
+		HPquiz.readQuestion("questions.txt", "gAnswers.txt", "hAnswers.txt", "rAnswers.txt", "sAnswers.txt");
 		HPquiz.display();
 		HPquiz.score();
 	}
